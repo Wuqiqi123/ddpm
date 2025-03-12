88e72deb74d21b9682d6bbb81b7967aa9696de4c
wqq <1258552199@qq.com>
2025-03-12 22:24:44 +0800

    udpate

---
+++ ddpm/unet.py
@@ -0,0 +1,5 @@
+import torch
+from torch import nn, einsum
+import torch.nn.functional as F
+from torch.nn import Module, ModuleList
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name = 'ddpm',
+    packages = find_packages(),
+    license='MIT',
+    version = '0.1.0',
+    description = 'Implementation of Diffusion Models'
+)
